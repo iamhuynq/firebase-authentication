7e1a4684101d8b8065f7370007130c697f93824c
huycan511 <nguyenquanghuy0511@gmail.com>
2020-01-17 13:32:38 +0700

    add signup

---
+++ scripts/auth.js
@@ -0,0 +1,14 @@
+const signUpForm = document.querySelector('#signup-form');
+signUpForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = signUpForm['signup-email'].value;
+    const password = signUpForm['signup-password'].value;
+
+    //sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signUpForm.reset();
+    })
+});
